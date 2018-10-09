e8527f502061bd3eff03d67ab45d16d051f17d68
O.K <okgit@protonmail.com>
2018-10-09 21:36:57 +0200

    Testing gitignore

--- Android/app/src/main/java/no/hiof/fridgebro/addActivity.java
+++ Android/app/src/main/java/no/hiof/fridgebro/addActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class addActivity extends AppCompatActivity {
 
+    // eid
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
