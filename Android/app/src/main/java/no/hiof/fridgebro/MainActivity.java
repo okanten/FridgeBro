df5807ac8b825f4e92a12aa652973ccd64ac68f5
Ola Wethal Petersen <olawp@hiof.no>
2018-10-09 21:08:08 +0200

    Minor changes

--- Android/app/src/main/java/no/hiof/fridgebro/MainActivity.java
+++ Android/app/src/main/java/no/hiof/fridgebro/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //TODO: Ola er homo
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
