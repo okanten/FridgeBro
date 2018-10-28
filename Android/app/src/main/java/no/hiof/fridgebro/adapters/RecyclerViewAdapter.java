54a67793cae1a9c20d0e40002474bf370f053e0a
O.K <okgit@protonmail.com>
2018-10-29 00:59:56 +0100

    Removed blank lines

--- Android/app/src/main/java/no/hiof/fridgebro/adapters/RecyclerViewAdapter.java
+++ Android/app/src/main/java/no/hiof/fridgebro/adapters/RecyclerViewAdapter.java
@@ -84,8 +84,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         });
     }
 
-
-
     @Override
     public int getItemCount() {
         return productList.size();
