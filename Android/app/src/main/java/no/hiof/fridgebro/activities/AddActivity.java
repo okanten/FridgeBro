9dfa599244ff522d3ad74cf3e298f27d3a112763
O.K <okgit@protonmail.com>
2018-10-29 00:57:59 +0100

    Added functionality to select from search result
    Opens a new dialog with search results which then lets the user pick which item they want to add

--- Android/app/src/main/java/no/hiof/fridgebro/activities/AddActivity.java
+++ Android/app/src/main/java/no/hiof/fridgebro/activities/AddActivity.java
@@ -2,14 +2,18 @@ package no.hiof.fridgebro.activities;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -23,19 +27,18 @@ import com.bumptech.glide.load.resource.bitmap.FitCenter;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.gson.JsonObject;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 import no.hiof.fridgebro.R;
 import no.hiof.fridgebro.adapters.RecyclerViewAdapter;
+import no.hiof.fridgebro.fragments.ContextMenuFragment;
 import no.hiof.fridgebro.fragments.RecyclerViewFragment;
 import no.hiof.fridgebro.models.Item;
 import no.hiof.olaka.*;
 
 
-public class AddActivity extends AppCompatActivity {
+public class AddActivity extends AppCompatActivity implements DialogInterface.OnDismissListener {
 
     private EditText txtISBN;
     private EditText txtPrice;
@@ -53,6 +56,7 @@ public class AddActivity extends AppCompatActivity {
     private Integer position;
     private Item newItem;
     private String itemDate;
+    private ContextMenuFragment contextMenuFragment;
 
 
     @Override
@@ -62,7 +66,7 @@ public class AddActivity extends AppCompatActivity {
         // TODO: Fikse up-activity/parentactivity
 
         txtISBN = (EditText) findViewById(R.id.txtISBN);
-        txtPrice = (EditText) findViewById(R.id.txtPrice);
+        txtPrice = (EditText) findViewById(R.id.contextPrice);
         imgItem = (ImageView) findViewById(R.id.imgItem);
         lblProductName = (TextView) findViewById(R.id.lblProductName);
         expDate = findViewById(R.id.expDate);
@@ -107,8 +111,6 @@ public class AddActivity extends AppCompatActivity {
 
     public void getPriceFromNg(View view) {
         String isbn = String.valueOf(txtISBN.getText());
-        //String isbn = String.valueOf(txtISBN.getText());
-
         new asyncLoadJson().execute(isbn);
     }
 
@@ -116,11 +118,26 @@ public class AddActivity extends AppCompatActivity {
         Toast.makeText(this, "Kunne ikke finne strekkode", Toast.LENGTH_SHORT).show();
     }
 
-    // TODO: Return metode / en måte å skille mellom shoppinglist og fridgelist.
-    // TODO: Vurdere alternativer til ngJson != null
+    public void openItemPickerDialog(ArrayList<Item> queryResult) {
+        FragmentManager fm = getSupportFragmentManager();
+        contextMenuFragment = ContextMenuFragment.newInstance(queryResult);
+        contextMenuFragment.show(fm, "fragment_context_menu");
+    }
+
+    public void setNewValues(ArrayList<Item> queryResult, int pos) {
+        newItem = queryResult.get(pos);
+        lblProductName.setText(queryResult.get(pos).getItemName());
+        txtPrice.setText(queryResult.get(pos).getItemPrice());
+        String imgUrl = queryResult.get(pos).getImageUrl();
+        Glide.with(getApplicationContext())
+                .load(imgUrl)
+                .apply(new RequestOptions().transform(new FitCenter()))
+                .into(imgItem);
+    }
+
     public void updateListOfItems(View view) {
-        if (ngJson != null) {
-            productList.add(new Item(ng.getCategoryName(null, ngJson), ng.getTitle(null, ngJson), String.valueOf(txtPrice.getText()), String.valueOf(txtISBN.getText()), ng.getImageURL(null, ngJson), String.valueOf(expDate.getText())));
+        if (newItem != null) {
+            productList.add(newItem);
             Intent resultIntent = new Intent();
             Bundle bundle = new Bundle();
             bundle.putInt("pos", position);
@@ -132,47 +149,50 @@ public class AddActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        /* Henter ut valgt element og setter nye verdier i viewet. */
+        setNewValues(contextMenuFragment.getQueryResult(), contextMenuFragment.getContextMenuAdapter().getPos());
+    }
 
 
-    public class asyncLoadJson extends AsyncTask<String, Integer, JsonObject> {
-        private JsonObject rJson;
+    public class asyncLoadJson extends AsyncTask<String, Integer, ArrayList<JsonObject>> {
+        private ArrayList<Item> queryResult = new ArrayList<>();
+        private ArrayList<JsonObject> rJson = new ArrayList<>();
 
         @Override
-        protected JsonObject doInBackground(String... strings) {
+        protected ArrayList<JsonObject> doInBackground(String... strings) {
             try {
-                rJson = new JsonObject();
-                /* Klar over at det blir overskrevet om flere blir requestet */
-                //for (String isbn: strings) {
-                rJson = ng.getJson(strings[0]);
-                newItem = new Item(ng.getCategoryName(null, rJson), ng.getTitle(null, rJson), ng.getPrice(null, rJson), strings[0], ng.getImageURL(null, rJson));
-                if (itemDate != null) {
-                    newItem.setExpDate(itemDate);
+                for (String isbn : strings) {
+                    rJson = ng.getFullJson(isbn);
+                    System.out.println(rJson.size());
                 }
-                System.out.println(rJson);
-                //}
+                System.out.println(rJson.size());
                 return rJson;
             } catch (RuntimeException re) {
                 return null;
             }
         }
 
-        protected void onPostExecute(JsonObject json) {
-            if (json != null) {
-                ngJson = json;
-                lblProductName.setText(ng.getTitle(null, json));
-                txtPrice.setText(ng.getPrice(null, json));
-                String imgUrl = ng.getImageURL(null, json);
-                Glide.with(getApplicationContext())
-                        .load(imgUrl)
-                        .apply(new RequestOptions().transform(new FitCenter()))
-                        .into(imgItem);
-                if (expDate != null) {
-                    //newItem.setExpDate();
+        protected void onPostExecute(ArrayList<JsonObject> rJson) {
+            System.out.println(ng.getImageURL(null, rJson.get(0)));
+            for (int i = 0; i < rJson.size(); i++) {
+                //productList.add(new Item("Test 1", "1", "11111", "https://i.redd.it/oir304dowbs11.jpg", "TestBrand", "03/03/2019"));
+                try {
+                    queryResult.add(new Item(ng.getTitle(null, rJson.get(i)), ng.getPrice(null, rJson.get(i)), ng.getISBN(null, rJson.get(i)), ng.getImageURL(null, rJson.get(i)), ng.getBrand(null, rJson.get(i))));
+                } catch (NullPointerException npe) {
+                    Toast.makeText(AddActivity.this, "Noe galt skjedde under søk. Vennligst redefiner søkeord", Toast.LENGTH_SHORT).show();
+                    Log.d("NullPointerException", npe.getMessage());
                 }
+            }
+            if (queryResult != null && queryResult.size() == 1) {
+                setNewValues(queryResult, 0);
+            } else if (queryResult.size() > 1) {
+                openItemPickerDialog(queryResult);
             } else {
                 makeToast();
             }
-        }
 
+        }
     }
 }
